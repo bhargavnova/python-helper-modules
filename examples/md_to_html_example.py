920c33a04b4c229c32f2b7f1f11d08ddfb94358c
Bhargav <bhargavkuvadiya117@gmail.com>
2023-10-03 08:34:58 +0530

    Revert "md_to_html: add script to convert markdown to html"

--- examples/md_to_html_example.py
+++
@@ -1,7 +0,0 @@
-from md_to_html import md_to_html
-
-if __name__ == "__main__":
-    input_file = input("Enter the input Markdown file name (e.g., input.md): ")
-    output_file = input("Enter the output HTML file name (e.g., output.html): ")
-
-    md_to_html.convert(input_file, output_file)
