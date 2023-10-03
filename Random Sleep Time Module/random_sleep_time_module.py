0a9428e805a944541db6f76437622d907b53eac1
Bhargav <bhargavkuvadiya117@gmail.com>
2023-10-03 10:57:55 +0530

    Update random_sleep_time_module.py

--- Random Sleep Time Module/random_sleep_time_module.py
+++ Random Sleep Time Module/random_sleep_time_module.py
@@ -4,10 +4,10 @@ import time
 import random
 import sys
 
-def random_sleep_time():
+def random_sleep_time(minTime, maxTime):
 
-    minTime=int(input("Enter the maximum time limit (in seconds): "))            # Making user input the left hand limit of sleep time
-    maxTime=int(input("Enter the upper time limit (in seconds): "))              # Making user input the right hand limit of sleep time 
+    #minTime=int(input("Enter the maximum time limit (in seconds): "))            # Making user input the left hand limit of sleep time
+    #maxTime=int(input("Enter the upper time limit (in seconds): "))              # Making user input the right hand limit of sleep time 
     randomTime=random.randint(minTime,maxTime)                                   # Choosing a random integer in between the time interval
     print("-> Sleeping for", randomTime, "Seconds")                              # Printing the random number chosen
 
