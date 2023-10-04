eba23e69436bf07488182f2e88d80c5d9ce064e2
Bhargav <bhargavkuvadiya117@gmail.com>
2023-10-04 08:21:06 +0530

    Update image_converter.py

--- Image Format Converter/image_converter.py
+++ Image Format Converter/image_converter.py
@@ -1,16 +1,6 @@
 #using aspose-words python module
 import aspose.words
 
-#file formats
-choose_format=["png", "jpeg", "jpg", "gif", "webp", "tiff"]
-
-#printing list to show popular options to user
-print(choose_format)
-
-cf=input("Type the format you want to convert to: ")
-inp=input("Type the file path which you want to convert: ")
-out=input("Type the file path to which you want to convert: ")
-
 #defning main function
 def convert_image(i,o,cf):
     doc = aspose.words.Document()
@@ -22,11 +12,23 @@ def convert_image(i,o,cf):
     s='.'
     k=s.join(l)
     shape.image_data.save(k)
-
+    
 #defining condition for unmatching image extension
 
-v=out.split('.')
-if cf==v[1]:
-    convert_image(inp,out,cf)
-else:
-    print('ERROR: chosen file format and saving file path extension do not match.')
+if __name__ == "__main__":
+
+    #file formats
+    choose_format=["png", "jpeg", "jpg", "gif", "webp", "tiff"]
+
+    #printing list to show popular options to user
+    print(choose_format)
+
+    cf=input("Type the format you want to convert to: ")
+    inp=input("Type the file path which you want to convert: ")
+    out=input("Type the file path to which you want to convert: ")
+
+    v=out.split('.')
+    if cf==v[1]:
+        convert_image(inp,out,cf)
+    else:
+        print('ERROR: chosen file format and saving file path extension do not match.')
