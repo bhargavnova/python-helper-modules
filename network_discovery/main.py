c5bac1889477c4e2b13d7f651620c928c9fc499c
Bhargav <bhargavkuvadiya117@gmail.com>
2023-10-04 08:52:21 +0530

    Update main.py

--- network_discovery/main.py
+++ network_discovery/main.py
@@ -1,7 +1,7 @@
 import network_discovery
 
 if __name__ == "__main__":
-    target_ip_range = "192.168.29.1/24"
+    target_ip_range = "xxx.xxx.xxx.xxx/24" #this will be using target local network ip, linux (ifconfig) and for windows (ipconfig), you can find it like that.
 
     print("Scanning for devices on the network...")
     devices = network_discovery.scan(target_ip_range)
